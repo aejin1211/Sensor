1c0046f15f7e5eb002b7149e1a4a7b5c8eefc637
aejin <dldowls1211@gmail.com>
2023-01-15 21:42:51 +0900

    Initial Commit

---
+++ Sensor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sensor
+//
+//  Created by 숙명pc on 2023/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
